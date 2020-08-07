e4ddc04085bee32e1044cddfe3030b2dd38db8df
Christian Legaard <clegaard@outlook.com>
2020-08-07 11:54:47 +0200

    Add height and width

--- clipper.py
+++ clipper.py
@@ -2,6 +2,8 @@ import logging
 import argparse
 from datetime import datetime
 from pathlib import Path
+import regex as re
+
 
 import pyperclip
 import pygments
@@ -35,6 +37,25 @@ class VSCodeLight(Style):
     }
 
 
+def infer_height_and_width(text: str, svg: str):
+
+    # count height by finding text element starting at largets y value. e.g. y="2000" in svg contents
+    max_height = max(
+        [int(y_str) for y_str in re.findall(pattern=r'<text.*y="([1-9]+)"', string=svg)]
+    )
+
+    # count maximum line width by looking at length substrings split by newline or carriage return
+    # assumption is that for a given fontsize the line width is directly proportional to the width of the rendered svg
+    fontsize = int(re.search(pattern=r'font-size="([0-9]+)px"', string=svg).group(1))
+    fontsize_to_width_ratio = 0.65  # tweaking parameter
+    assert fontsize > 0
+    max_width = int(
+        max([len(t) for t in text.splitlines()]) * fontsize * fontsize_to_width_ratio
+    )
+
+    return (max_height, max_width)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(prog="clipper")
@@ -43,6 +64,12 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--formatter", default="svg", help="formatter to use for exporting code"
+    )
+
+    parser.add_argument(
+        "--crop",
+        action="store_true",
+        help="try to crop the svg file to fit the highlighted code",
     )
 
     parser.add_argument(
@@ -79,11 +106,25 @@ if __name__ == "__main__":
             ts = datetime.timestamp(datetime.now())
             outfile = outdir / f"{ts}.svg"
 
+            svg = pygments.highlight(code=s, lexer=lexer, formatter=formatter)
+
+            if args.crop:
+                height, width = infer_height_and_width(text=s, svg=svg)
+                logger.info(f"cropping svg to height:{height} width:{width}")
+                from regex import sub
+
+                svg = sub(
+                    pattern="<svg",
+                    repl=f'<svg height="{height}" width="{width}" ',
+                    string=svg,
+                )
+
             with open(outfile, "w") as f:
-                out = pygments.highlight(code=s, lexer=lexer, formatter=formatter)
-                f.write(out)
-                if args.overwrite_clipboard:
-                    pyperclip.copy(out)
+
+                f.write(svg)
+
+            if args.overwrite_clipboard:
+                pyperclip.copy(svg)
 
             logger.info(f"Snippet stores as: {outfile}")
 
