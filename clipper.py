cacbe0c69c57870de567a8241ea833a9d59eb397
Christian Legaard <clegaard@outlook.com>
2020-08-07 13:02:26 +0200

    Tweak theme to be closer to Light (Visual Studio)

--- clipper.py
+++ clipper.py
@@ -17,6 +17,7 @@ from pygments.token import (
     Number,
     Operator,
     Generic,
+    Whitespace,
 )
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import get_formatter_by_name
@@ -25,15 +26,48 @@ from pygments.styles import get_style_by_name
 logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.DEBUG)
 
-# Replicate the "Light (VSCode)"-theme in pygments
+#
+# class VSCodeLight(Style):
+#     styles = {
+#         Comment: "#008000",
+#         Keyword: "#0000FF",
+#         # Name: "#f00",
+#         # Name.Function: "#0f0",
+#         # Name.Class: "bold #0f0",
+#         String: "#A31515",
+#     }
+
+
 class VSCodeLight(Style):
+    """
+    Replicate the "Light (Visual Studio)"-theme in pygments
+    """
+
+    default_style = ""
+
     styles = {
+        Whitespace: "#bbbbbb",
         Comment: "#008000",
-        Keyword: "#0000FF",
-        # Name: "#f00",
-        # Name.Function: "#0f0",
-        # Name.Class: "bold #0f0",
+        Comment.Preproc: "noitalic #008080",
+        Comment.Special: "noitalic bold",
         String: "#A31515",
+        String.Char: "#800080",
+        Number: "#098658",
+        Keyword: "#0000FF",
+        Operator.Word: "bold",
+        Name.Tag: "bold #000080",
+        Name.Attribute: "#FF0000",
+        Generic.Heading: "#999999",
+        Generic.Subheading: "#aaaaaa",
+        Generic.Deleted: "bg:#ffdddd #000000",
+        Generic.Inserted: "bg:#ddffdd #000000",
+        Generic.Error: "#aa0000",
+        Generic.Emph: "italic",
+        Generic.Strong: "bold",
+        Generic.Prompt: "#555555",
+        Generic.Output: "#888888",
+        Generic.Traceback: "#aa0000",
+        Error: "bg:#e3d2d2 #a61717",
     }
 
 
@@ -93,7 +127,7 @@ if __name__ == "__main__":
     while True:
 
         lexer = get_lexer_by_name(args.lexer)
-        formatter = get_formatter_by_name(args.formatter, line_numbers=True,)
+        formatter = get_formatter_by_name(args.formatter, style=VSCodeLight)
 
         logger.info("waiting for new paste")
         s = pyperclip.waitForNewPaste()
